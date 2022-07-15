ae10b4647be1d7799470a055ff6f1d5127fd4d37
wanjuhiann <106369744+wanjuhiann@users.noreply.github.com>
2022-07-15 22:49:44 +0300

    Add files via upload

---
+++ Ann nyambura Quadratic.c
@@ -0,0 +1,34 @@
+/*
+project Quadratic
+Created by Ann nyambura
+C89
+MIT
+July 14
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{   float a,b,c
+    float root1,root2
+    float root-part,denom;
+    printf("Enter values of a,b, and c/n");
+    scanf("%f%f%f,&a,&b,&c");
+
+
+    root-part=sqrt(b*b-4*a*c);
+    denom  =2*a;
+
+      root1 =(-b+root_part)/denom;
+      root2 =(-b-root_part)/denom;
+
+      printf(Root1 = %f /nRoot2=%f",root1,root2);
+
+
+      return 0;
+
+
+
+    return 0;
+}
